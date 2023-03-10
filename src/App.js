184c15db583039937594e2d7fb20006c0d6bc049
Prince Masuku <Prince.Masuku@investec.co.za>
2023-03-10 08:51:39 +0200

    meetup app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-
-
+import { Route, Switch } from "react-router-dom";
+import AllMeetUpsPage from "./pages/AllMeetups";
+import NewMeetUpPage from "./pages/NewMeetup";
+import FavouritesPage from "./pages/Favourites";
+import Layout from "./components/layout/Layout";
 function App() {
   return (
-    <div >
-      Hello!
-    </div>
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetUpsPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetUpPage />
+        </Route>
+        <Route path="/favourites">
+          <FavouritesPage />
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
 
